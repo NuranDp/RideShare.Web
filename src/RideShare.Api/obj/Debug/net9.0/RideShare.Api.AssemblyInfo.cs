c50158170968fb8e9f25df7a1db737d0101998b5
Mohtasim Nuran <mnuran@julyservices.com>
2026-03-15 04:13:01 +0600

    Update assembly informational version and cache files for RideShare.Api and RideShare.Core

--- src/RideShare.Api/obj/Debug/net9.0/RideShare.Api.AssemblyInfo.cs
+++ src/RideShare.Api/obj/Debug/net9.0/RideShare.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RideShare.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44d819d34e4361231caf63b2e8b646c7e5f49c24")]
 [assembly: System.Reflection.AssemblyProductAttribute("RideShare.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RideShare.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
